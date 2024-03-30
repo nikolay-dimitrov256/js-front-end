b0b09a741dbc2f60f29d29a304332805788c79a7
Nikolay Dimitrov <141030298+nikolay-dimitrov256@users.noreply.github.com>
2024-03-30 13:34:16 +0200

    Update app.js

--- DOM Manipulation and Events Lab/02. Delete-From-Table/app.js
+++ DOM Manipulation and Events Lab/02. Delete-From-Table/app.js
@@ -1,5 +1,5 @@
 function deleteByEmail() {
-    const tableTbodyElement = document.querySelector('#customers tbody');
+    const tableTbodyElement = document.querySelector('#customers tbody'); // Redundant
     const tableTrElements = document.querySelectorAll('#customers tbody tr');
     const inputFieldElement = document.querySelector('input[name=email]');
     const resultElement = document.getElementById('result');
